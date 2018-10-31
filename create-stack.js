bb3087cb4b7b139fdcd17086f8b0df1d4d0bf61b
Andrew Brown <andrew@monsterboxpro.com>
2018-10-30 21:34:54 -0400

    More changes

--- create-stack.js
+++ create-stack.js
@@ -1,28 +1,35 @@
 const { exec }      = require('child_process')
 const fs            = require('fs')
 const path          = require('path')
-const stack_name    = 'ServerlessContactFormXX'
 const template_path = path.join(__dirname,'template.yaml')
 
-fs.readFile('create-stack.config', 'utf8', function(err, contents) {
-  const lines = contents.split("\n")
-  lines.pop()
-  let params = ''
-  let val
-  lines.forEach(function(line) {
-    val = line.split('=')
-    params += `ParameterKey=${val[0]},ParameterValue=${val[1]} `
+require('crypto').randomBytes(12, function(err, buffer) {
+  var token = buffer.toString('hex');
+  const stack_name    = `ServerlessContactForm-${token}`
+
+  fs.readFile('create-stack.config', 'utf8', function(err, contents) {
+    const lines = contents.split("\n")
+    lines.pop()
+    let params = ''
+    let val
+    lines.forEach(function(line) {
+      val = line.split('=')
+      params += `ParameterKey=${val[0]},ParameterValue=${val[1]} `
+    })
+
+    const command = `aws cloudformation create-stack --stack-name ${stack_name} --template-body file://${template_path} --parameters ${params} --capabilities CAPABILITY_IAM`
+
+    console.log(command)
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      console.log(`${stderr}`)
+      console.log(`${stdout}`)
+    })
   })
 
-  const command = `aws cloudformation create-stack --stack-name ${stack_name} --template-body file://${template_path} --parameters ${params} --capabilities CAPABILITY_IAM`
 
-  console.log(command)
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      console.log(err)
-      return
-    }
-    console.log(`${stderr}`)
-    console.log(`${stdout}`)
-  })
-})
+});
+
